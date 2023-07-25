b04a4dffcab179c118567aaea251cf4fef5e9453
Stella Laurenzo <stellaraccident@gmail.com>
2023-07-24 22:41:43 -0700

    Add initial job to build/test with assertions.

--- build_tools/ci/linux_default_toolchain.cmake
+++ build_tools/ci/linux_default_toolchain.cmake
@@ -8,6 +8,11 @@ set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld -Wl,--gdb-index")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld -Wl,--gdb-index")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld -Wl,--gdb-index")
 
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -gsplit-dwarf -ggnu-pubnames")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -gsplit-dwarf -ggnu-pubnames")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} -gsplit-dwarf -ggnu-pubnames")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -gsplit-dwarf -ggnu-pubnames")
+
 set(CLANG_ENABLE_ARCMT OFF CACHE BOOL "" FORCE)
 set(CLANG_ENABLE_STATIC_ANALYZER OFF CACHE BOOL "" FORCE)
 
