cdca91e4791573ec311dd26451953779dc14db66
Arham Khan <arhammkhan@gmail.com>
2023-08-10 11:48:24 -0400

    add default decompositions to generated test suite

--- python/test/generated/evaluate.py
+++ python/test/generated/evaluate.py
@@ -9,22 +9,51 @@ from torch.fx import (
     GraphModule,
 )
 
+from torch.fx.experimental.proxy_tensor import make_fx
+from torch._decomp import get_decompositions
+from torch.func import functionalize
+from typing import List
+
+def default_decompositions():
+    return get_decompositions(
+        [
+            torch.ops.aten.embedding_dense_backward,
+            torch.ops.aten.native_layer_norm_backward,
+            torch.ops.aten.slice_backward,
+            torch.ops.aten.select_backward,
+            torch.ops.aten.norm.ScalarOpt_dim,
+            torch.ops.aten.native_group_norm,
+            torch.ops.aten.upsample_bilinear2d.vec,
+            torch.ops.aten.split.Tensor,
+            torch.ops.aten.split_with_sizes,
+            torch.ops.aten.native_layer_norm,
+            torch.ops.aten.masked_fill.Tensor,
+            torch.ops.aten.masked_fill.Scalar,
+        ]
+    )
+
+
 def create_backend():
     imp = FxImporter()
 
     def import_compiler(gm: GraphModule, example_inputs):
-        # gm.print_readable()
+        gm = make_fx(
+            functionalize(gm),
+            decomposition_table=default_decompositions(),
+        )(*example_inputs)
+
+        gm.print_readable()
         try:
             imp.import_graph_module(gm)
         finally:
-            pass
-            # print(imp.module)
+            print(imp.module)
         imp.module.operation.verify()
         return gm
 
     backend = import_compiler
     backend = aot_autograd(fw_compiler=backend)
     return backend
+
 
 def evaluate_importer(nn_cls, get_init_args, get_forward_args, test_identifier):
     log = logging.getLogger("turbine-test")
