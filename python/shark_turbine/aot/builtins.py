e6b9cad1465b392688f6001501fc141c2dca076d
Stella Laurenzo <stellaraccident@gmail.com>
2023-09-03 20:09:27 -0700

    Sync copyright from iree-jax

--- python/shark_turbine/aot/builtins.py
+++ python/shark_turbine/aot/builtins.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Nod Labs, Inc
+# Portions Copyright 2022 The IREE Authors
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
