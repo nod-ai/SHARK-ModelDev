2397a0c69cd8807ceff0a796ea465ef4e1f6623d
Stella Laurenzo <stellaraccident@gmail.com>
2023-09-06 18:14:46 -0700

    Fix some tests that fail on old Linux

--- python/test/dynamo/backend_smoke_test.py
+++ python/test/dynamo/backend_smoke_test.py
@@ -7,11 +7,11 @@
 import torch
 
 
-def foo(x, y):
-    a = torch.sin(x)
-    b = torch.cos(y)
-    return a + b
+def test_basic():
+    def foo(x, y):
+        a = torch.sin(x)
+        b = torch.cos(y)
+        return a + b
 
-
-opt_foo1 = torch.compile(foo, backend="turbine_cpu")
-print(opt_foo1(torch.randn(10, 10), torch.randn(10, 10)))
+    opt_foo1 = torch.compile(foo, backend="turbine_cpu")
+    print(opt_foo1(torch.randn(10, 10), torch.randn(10, 10)))
