349c06fb88520b20e7e4c35d7ee8bee5dec43261
Stella Laurenzo <stellaraccident@gmail.com>
2023-09-14 18:19:03 -0700

    Implement export and management of global state trees. (#51)

--- python/shark_turbine/aot/builtins/globals.py
+++ python/shark_turbine/aot/builtins/globals.py
@@ -28,7 +28,7 @@ from ..support import (
 class export_global(GlobalsDef, Abstractifiable):
     """Exports a single global into a CompiledModule."""
 
-    __slots__ = ["_name", "_value"]
+    __slots__ = ["_name", "_value", "_schema"]
 
     def __init__(
         self,
@@ -41,12 +41,46 @@ class export_global(GlobalsDef, Abstractifiable):
         super().__init__(initialize=initialize, mutable=mutable)
         self._name = name
         self._value = value
+        _, self._schema = tree_flatten(self._value)
 
     def items(self):
         yield (self._name, self._value)
 
+    def schema(self) -> TreeSpec:
+        return self._schema
+
     def abstractify(self) -> AbstractTypedef:
         return abstractify_single_value(self._value)
+
+
+class export_global_tree(GlobalsDef, Abstractifiable):
+    """Exports a tree of globals into a CompiledModule."""
+
+    def __init__(
+        self,
+        tree,
+        *,
+        name: str = "global",
+        initialize: bool = True,
+        mutable: bool = False,
+    ):
+        super().__init__(initialize=initialize, mutable=mutable)
+        self._tree = tree
+        self._items, self._schema = tree_flatten(tree)
+        self._names, _ = tree_flatten(_transform_tree_to_names("", tree))
+        assert len(self._items) == len(
+            self._names
+        ), f"Name and value tree are different sizes: {len(self._items)} != {len(self._names)}"
+
+    def items(self):
+        for name, value in zip(self._names, self._items):
+            yield name, value
+
+    def schema(self) -> TreeSpec:
+        return self._schema
+
+    def abstractify(self) -> AbstractTypedef:
+        return tree_map(abstractify_single_value, self._tree)
 
 
 class export_parameters(GlobalsDef, TreeAbstractifiable):
@@ -85,3 +119,21 @@ class export_parameters(GlobalsDef, TreeAbstractifiable):
     def __repr__(self):
         names = [name for name, _ in self._param_list]
         return f"<export_parameters {', '.join(names)}>"
+
+
+def _transform_tree_to_names(prefix: str, tree):
+    """Produces a topologically similar tree but where each value is a fully qualified name."""
+    join = lambda key: f"{prefix}.{key}" if prefix else key
+    # No need to check for cycles as pytree already did something with it and
+    # validates.
+    if isinstance(tree, dict):
+        return tree.__class__(
+            (k, _transform_tree_to_names(join(k), v)) for k, v in tree.items()
+        )
+    elif isinstance(tree, (list, tuple)):
+        return tree.__class__(
+            _transform_tree_to_names(join(str(index)), v)
+            for index, v in enumerate(tree)
+        )
+    else:
+        return prefix
