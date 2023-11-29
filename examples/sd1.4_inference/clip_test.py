1d1c1a152891206edd391c72954c54cb7019bbee
aviator19941 <avinash@nod-labs.com>
2023-11-29 23:35:46 +0000

    Load weights at runtime for CLIP

--- examples/sd1.4_inference/clip_test.py
+++ examples/sd1.4_inference/clip_test.py
@@ -4,12 +4,13 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-import logging
-import unittest
+import os
+import sys
+import re
 
+from iree import runtime as ireert
 from iree.compiler.ir import Context
 import numpy as np
-import re
 from shark_turbine.aot import *
 import torch
 import torch._dynamo as dynamo
@@ -23,15 +24,18 @@ import safetensors
 import argparse
 
 parser = argparse.ArgumentParser()
-
+parser.add_argument("--run_vmfb", action="store_true")
 parser.add_argument("--compile_to", type=str, help="torch, linalg, vmfb")
 parser.add_argument("--external_weight_file", type=str, default="")
+parser.add_argument("--vmfb_path", type=str, default="")
 parser.add_argument(
     "--external_weights",
     type=str,
     default=None,
     help="saves ir/vmfb without global weights for size and readability, options [safetensors]",
 )
+
+prompt = ["a photograph of an astronaut riding a horse"]
 
 def export_transformer_model(
     compile_to,
@@ -114,15 +118,82 @@ def export_transformer_model(
         exit()
 
 
-if __name__ == "__main__":
-    args = parser.parse_args()
-    mod_str, _ = export_transformer_model(
-        args.compile_to,
-        args.external_weights,
-        args.external_weight_file,
-    )
+def largest_error(array1, array2):
+    absolute_diff = np.abs(array1 - array2)
+    max_error = np.max(absolute_diff)
+    return max_error
+
+
+def run_vmfb_comparison(args):
+    config = ireert.Config("local-task")
+
+    if args.external_weight_file:
+        index = ireert.ParameterIndex()
+        index.load(args.external_weight_file)
+
     safe_name = pretrained_model_name_or_path.split("/")[-1].strip()
     safe_name = re.sub("-", "_", safe_name)
-    with open(f"{safe_name}.mlir", "w+") as f:
-        f.write(mod_str)
-    print("Saved to", safe_name + ".mlir")
+    if args.vmfb_path:
+        mod = ireert.VmModule.mmap(config.vm_instance, args.vmfb_path)
+    elif os.path.exists(f"{safe_name}.vmfb"):
+        mod = ireert.VmModule.mmap(config.vm_instance, f"{safe_name}.vmfb")
+    else:
+        sys.exit("no vmfb_path provided, required for run_vmfb")
+
+    vm_modules = [
+        mod,
+        ireert.create_hal_module(config.vm_instance, config.device),
+    ]
+    if args.external_weight_file:
+        param_module = ireert.create_io_parameters_module(
+            config.vm_instance, index.create_provider(scope="model")
+        )
+        vm_modules.insert(0, param_module)
+    
+    ctx = ireert.SystemContext(
+        vm_modules=vm_modules,
+        config=config,
+    )
+    tokenizer = CLIPTokenizer.from_pretrained(
+        pretrained_model_name_or_path,
+        subfolder="tokenizer"
+    )
+    text_input = tokenizer(prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
+    inp = text_input.input_ids
+    device_inputs = [ireert.asdevicearray(config.device, inp)]
+
+    # Turbine output
+    ModuleCompiled = ctx.modules.compiled_clip
+    turbine_outputs = ModuleCompiled["main"](*device_inputs)
+    turbine_output = turbine_outputs[0]
+    print(turbine_output.to_host(), turbine_output.to_host().shape, turbine_output.to_host().dtype)
+
+    # Torch output
+    text_encoder_model = CLIPTextModel.from_pretrained(
+        pretrained_model_name_or_path,
+        subfolder="text_encoder"
+    )
+    torch_output = text_encoder_model.forward(inp)[0]
+    torch_output = torch_output.detach().cpu().numpy()
+    print(torch_output, torch_output.shape, torch_output.dtype)
+
+    err = largest_error(torch_output, turbine_output)
+    print('LARGEST ERROR:', err)
+    assert(err < 9e-5)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.run_vmfb:
+        run_vmfb_comparison(args)
+    else:
+        mod_str, _ = export_transformer_model(
+            args.compile_to,
+            args.external_weights,
+            args.external_weight_file,
+        )
+        safe_name = pretrained_model_name_or_path.split("/")[-1].strip()
+        safe_name = re.sub("-", "_", safe_name)
+        with open(f"{safe_name}.mlir", "w+") as f:
+            f.write(mod_str)
+        print("Saved to", safe_name + ".mlir")
