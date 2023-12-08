7baababa8dcc346aab1f5013914ae505fa64a201
Zach Garvey <zjgarvey@gmail.com>
2023-12-08 12:39:28 -0600

    Cleaned up Resnet Example

--- examples/resnet-18/resnet-18.py
+++ examples/resnet-18/resnet-18.py
@@ -1,78 +1,79 @@
-import os
 import numpy as np
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 import torch
 from shark_turbine.aot import *
 from iree.compiler.ir import Context
 from iree.compiler.api import Session
+import iree.runtime as rt
 from datasets import load_dataset
-import PIL
 
-# Load feature extractor and pretrained model from huggingface
+# Loading feature extractor and pretrained model from huggingface
 extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
 model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-18")
-
-###
-#standard inference example (no SHARK-Turbine):
-###
 
 #load an example
 dataset = load_dataset("huggingface/cats-image")
 image = dataset["test"]["image"][0]
 
-#image.save("cats-image.jpg") #if you want to see the cute picture :)
+#if you want to see the cat picture:
+#image.save("cats-image.jpg") 
 
-#uncomment the line below to run your own image through inference.
+#if you want to run a custom image through inference.
+#import PIL
 #image = PIL.JpegImagePlugin.JpegImageFile("yourexamplepicture.jpg")
 
 #extract features from image to feed to model
 inputs = extractor(image, return_tensors="pt")
 pixel_tensor = inputs.pixel_values
 
-#standard torch application of model to extracted feature input.
-def std_infer(pixel_values_tensor: torch.Tensor):
+#define a function to do inference
+#this will get passed to the compiled module as a jittable function
+def forward(pixel_values_tensor: torch.Tensor):
     with torch.no_grad():
         logits = model.forward(pixel_values_tensor).logits
-    predicted_label = torch.argmax(logits,-1).item() 
-    textout = model.config.id2label[predicted_label]
-    return textout
+    predicted_id = torch.argmax(logits,-1)
+    return predicted_id
 
-###
-#Using SHARK-Turbine to write a torch-mlir file for performing inference with the model.
-###
+#A dynamic module for doing inference
+class RN18(CompiledModule):
+    params = export_parameters(model)
 
-class wtfamidoing2(CompiledModule):
-    #this one is trying to pass the pixel_value tensor as an Abstract Tensor?
-    params = export_parameters(model, initialize = True) #         is this necessary? 
+    def forward(self, x = AbstractTensor(None, 3, 224, 224, dtype=torch.float32) ):
+        #set a constraint for the dynamic number of batches
+        const = [x.dynamic_dim(0) < 16]
+        return jittable(forward)(x,constraints=const) 
 
-    def forward(self, x = AbstractTensor(1, 3, 224, 224, dtype=torch.float32) ):
-        with torch.no_grad():
-            logits0 = jittable(model.forward)(x)['logits']
-        label = self.labelout(logits0)
-        #str_output = model.config.id2label[predicted_label0] 
-        return label
+#build an instance of our CompiledModule and get the corresponding mlir
+inst2 = RN18(context=Context(), import_to="IMPORT")
 
-    @jittable
-    def labelout(logitsin):
-        label =  torch.argmax(logitsin,-1)
-        return label
-
-#build an instance of our CompiledModule and get the corresponding mlir code
-inst2 = wtfamidoing2(context=Context(), import_to="IMPORT")
-
+#if you want to view the mlir output:
 #module_str2 = str(CompiledModule.get_mlir_module(inst2))
 #print(module_str2)
+#with open('resnet-18.mlir','w+') as f:
+#    f.write(module_str2)
 
 session = Session()
 ExportedModule = exporter.ExportOutput(session,inst2)
 compiled_binary = ExportedModule.compile(save_to=None)
 
+#return type is rt.array_interop.DeviceArray
+#np.array of outputs can be accessed via to_host() method
 def shark_infer(x):
-    import iree.runtime as rt
     config = rt.Config("local-task")
     vmm = rt.load_vm_module(
         rt.VmModule.wrap_buffer(config.vm_instance, compiled_binary.map_memory()),
-        config,
+        config
     )
-    y = vmm.main(x)
-    return y
+    y = vmm.forward(x)
+    return y 
+
+#prints the text labels for output ids
+def print_labels(id):
+    for num in id : print(model.config.id2label[num])
+
+#not sure what the point was of the dynamic dim constraint 
+#also amusing that random tensors are always jellyfish
+x = torch.randn(17,3,224,224)
+x[2] = pixel_tensor
+y = shark_infer(x)
+print_labels(y.to_host())
