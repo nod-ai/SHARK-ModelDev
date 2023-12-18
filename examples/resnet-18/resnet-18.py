e0942991d6d09d94c1e7180eb43b72bc929e76f4
Zach Garvey <zjgarvey@gmail.com>
2023-12-18 17:49:23 -0600

    ran black for formatting

--- examples/resnet-18/resnet-18.py
+++ examples/resnet-18/resnet-18.py
@@ -7,6 +7,7 @@ import iree.runtime as rt
 extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
 model = AutoModelForImageClassification.from_pretrained("microsoft/resnet-18")
 
+
 # define a function to do inference
 # this will get passed to the compiled module as a jittable function
 def forward(pixel_values_tensor: torch.Tensor):
@@ -14,6 +15,7 @@ def forward(pixel_values_tensor: torch.Tensor):
         logits = model.forward(pixel_values_tensor).logits
     predicted_id = torch.argmax(logits, -1)
     return predicted_id
+
 
 # a dynamic module for doing inference
 # this will be compiled AOT to a memory buffer
@@ -26,10 +28,12 @@ class RN18(CompiledModule):
         const = [x.dynamic_dim(0) < 16]
         return jittable(forward)(x, constraints=const)
 
+
 # build an mlir module with 1-shot exporter
 exported = export(RN18)
 # compile exported module to a memory buffer
 compiled_binary = exported.compile(save_to=None)
+
 
 # return type is rt.array_interop.DeviceArray
 # np.array of outputs can be accessed via to_host() method
@@ -42,14 +46,17 @@ def shark_infer(x):
     y = vmm.forward(x)
     return y
 
+
 # prints the text corresponding to output label codes
 def print_labels(id):
     for l in id:
         print(model.config.id2label[l])
 
+
 # finds discrepancies between id0 and id1
 def compare_labels(id0, id1):
     return (id0 != id1).nonzero(as_tuple=True)
+
 
 # load some examples and check for discrepancies between
 # compiled module and standard inference (forward function)
@@ -61,9 +68,9 @@ import random
 BATCH_SIZE = 100
 dataset = load_dataset("cats_vs_dogs")
 # extract a window of examples.
-start_index = random.randint(0,23443 - BATCH_SIZE)
+start_index = random.randint(0, 23443 - BATCH_SIZE)
 end_index = start_index + BATCH_SIZE
-image = dataset['train'][start_index:end_index]["image"]
+image = dataset["train"][start_index:end_index]["image"]
 # convert images to a torch.Tensor(BATCH_SIZE,3,224,224)
 inputs = extractor(image, return_tensors="pt")
 pixel_tensor = inputs.pixel_values
@@ -74,5 +81,7 @@ y1 = shark_infer(pixel_tensor)
 y2 = forward(pixel_tensor)
 print("Inference completed. Here are a few sample labels. \n")
 print_labels(y1.to_host()[0:5])
-diff = compare_labels(y1,y2)
-print(f'\nDiscrepancies between SharkTurbine and Standard inference results: {diff[0].size()[0]}')
+diff = compare_labels(y1, y2)
+print(
+    f"\nDiscrepancies between SharkTurbine and Standard inference results: {diff[0].size()[0]}"
+)
