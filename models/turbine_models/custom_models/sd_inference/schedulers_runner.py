cccf5768d3f4ea8c9bcf28b61735bb9aa4d94dd6
aviator19941 <aviator1994@gmail.com>
2024-02-06 23:37:42 +0000

    Add AMD copyrights

--- models/turbine_models/custom_models/sd_inference/schedulers_runner.py
+++ models/turbine_models/custom_models/sd_inference/schedulers_runner.py
@@ -1,3 +1,9 @@
+# Copyright 2024 Advanced Micro Devices, Inc
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import argparse
 from turbine_models.model_runner import vmfbRunner
 from iree import runtime as ireert
