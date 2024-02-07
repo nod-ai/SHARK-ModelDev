44d6f165ceac916c4928a56f307e8ee18d58c3fa
Ean Garvey <garveyej@gmail.com>
2024-02-07 10:14:47 -0600

    Move SDXL scripts.

---
+++ models/turbine_models/custom_models/sdxl_inference/clip.py
@@ -0,0 +1,165 @@
+# Copyright 2023 Nod Labs, Inc
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import os
+import sys
+
+from iree import runtime as ireert
+import iree.compiler as ireec
+from iree.compiler.ir import Context
+import numpy as np
+from shark_turbine.aot import *
+from turbine_models.custom_models.sd_inference import utils
+import torch
+import torch._dynamo as dynamo
+from transformers import CLIPTextModel, CLIPTextModelWithProjection, CLIPTokenizer
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--hf_auth_token", type=str, help="The Hugging Face auth token, required"
+)
+parser.add_argument(
+    "--hf_model_name",
+    type=str,
+    help="HF model name",
+    default="stabilityai/sdxl-turbo",
+)
+parser.add_argument("--compile_to", type=str, help="torch, linalg, vmfb")
+parser.add_argument("--external_weight_path", type=str, default="")
+parser.add_argument(
+    "--external_weights",
+    type=str,
+    default=None,
+    help="saves ir/vmfb without global weights for size and readability, options [safetensors]",
+)
+parser.add_argument("--device", type=str, default="cpu", help="cpu, cuda, vulkan, rocm")
+# TODO: Bring in detection for target triple
+parser.add_argument(
+    "--iree_target_triple",
+    type=str,
+    default="",
+    help="Specify vulkan target triple or rocm/cuda target device.",
+)
+parser.add_argument("--vulkan_max_allocation", type=str, default="4294967296")
+
+
+def export_clip_model(
+    hf_model_name,
+    hf_auth_token=None,
+    compile_to="torch",
+    external_weights=None,
+    external_weight_path=None,
+    device=None,
+    target_triple=None,
+    max_alloc=None,
+):
+    # Load the tokenizer and text encoder to tokenize and encode the text.
+    tokenizer_1 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer",
+        token=hf_auth_token,
+    )
+    text_encoder_1_model = CLIPTextModel.from_pretrained(
+        hf_model_name,
+        subfolder="text_encoder",
+        token=hf_auth_token,
+    )
+    tokenizer_2 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer_2",
+        token=hf_auth_token,
+    )
+    text_encoder_2_model = CLIPTextModelWithProjection.from_pretrained(
+        hf_model_name,
+        subfolder="text_encoder_2",
+        token=hf_auth_token,
+    )
+    mapper = {}
+    if external_weight_path:
+        weights_path_1 = external_weight_path.split(f".{external_weights}")[0] + "_1" + f".{external_weights}"
+        weights_path_2 = external_weight_path.split(f".{external_weights}")[0] + "_2" + f".{external_weights}"
+    else:
+        weights_path_1 = None
+        weights_path_2 = None
+    
+    utils.save_external_weights(
+        mapper, text_encoder_1_model, external_weights, weights_path_1
+    )
+    utils.save_external_weights(
+        mapper, text_encoder_2_model, external_weights, weights_path_2
+    )
+
+    class CompiledClip1(CompiledModule):
+        if external_weights:
+            params = export_parameters(
+                text_encoder_1_model,
+                external=True,
+                external_scope="",
+                name_mapper=mapper.get,
+            )
+        else:
+            params = export_parameters(text_encoder_1_model)
+
+        def main(self, inp=AbstractTensor(1, 77, dtype=torch.int64)):
+            return jittable(text_encoder_1_model.forward)(inp)
+    
+    class CompiledClip2(CompiledModule):
+        if external_weights:
+            params = export_parameters(
+                text_encoder_2_model,
+                external=True,
+                external_scope="",
+                name_mapper=mapper.get,
+            )
+        else:
+            params = export_parameters(text_encoder_2_model)
+
+        def main(self, inp=AbstractTensor(1, 77, dtype=torch.int64)):
+            return jittable(text_encoder_2_model.forward)(inp)
+
+    import_to = "INPUT" if compile_to == "linalg" else "IMPORT"
+    inst1 = CompiledClip1(context=Context(), import_to=import_to)
+    inst2 = CompiledClip2(context=Context(), import_to=import_to)
+
+    module_1_str = str(CompiledModule.get_mlir_module(inst1))
+    module_2_str = str(CompiledModule.get_mlir_module(inst2))
+    safe_name_1 = utils.create_safe_name(hf_model_name, "-clip-1")
+    safe_name_2 = utils.create_safe_name(hf_model_name, "-clip-2")
+    if compile_to != "vmfb":
+        return module_1_str, module_2_str, tokenizer_1, tokenizer_2
+    else:
+
+        vmfb_path_1 = utils.compile_to_vmfb(module_1_str, device, target_triple, max_alloc, safe_name_1, return_path=True)
+        vmfb_path_2 = utils.compile_to_vmfb(module_2_str, device, target_triple, max_alloc, safe_name_2, return_path=True)
+
+        return vmfb_path_1, vmfb_path_2, tokenizer_1, tokenizer_2
+
+
+if __name__ == "__main__":
+    import re
+    args = parser.parse_args()
+    mod_1_str, mod_2_str, _, _ = export_clip_model(
+        args.hf_model_name,
+        args.hf_auth_token,
+        args.compile_to,
+        args.external_weights,
+        args.external_weight_path,
+        args.device,
+        args.iree_target_triple,
+        args.vulkan_max_allocation,
+    )
+    safe_name = args.hf_model_name.split("/")[-1].strip()
+    safe_name = re.sub("-", "_", safe_name)
+    safe_name_1 = safe_name + "_clip_1"
+    safe_name_2 = safe_name + "_clip_2"
+    with open(f"{safe_name_1}.mlir", "w+") as f:
+        f.write(mod_1_str)
+    print("Saved to", safe_name_1 + ".mlir")
+    with open(f"{safe_name_2}.mlir", "w+") as f:
+        f.write(mod_2_str)
+    print("Saved to", safe_name_2 + ".mlir")
---
+++ models/turbine_models/custom_models/sdxl_inference/vae.py
@@ -0,0 +1,171 @@
+# Copyright 2023 Nod Labs, Inc
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import os
+import sys
+
+from iree import runtime as ireert
+from iree.compiler.ir import Context
+import numpy as np
+from shark_turbine.aot import *
+from turbine_models.custom_models.sd_inference import utils
+import torch
+import torch._dynamo as dynamo
+from diffusers import AutoencoderKL
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--hf_model_name",
+    type=str,
+    help="HF model name",
+    default="stabilityai/sdxl-turbo",
+)
+parser.add_argument(
+    "--batch_size", type=int, default=1, help="Batch size for inference"
+)
+parser.add_argument(
+    "--height", type=int, default=1024, help="Height of Stable Diffusion"
+)
+parser.add_argument("--width", type=int, default=1024, help="Width of Stable Diffusion")
+parser.add_argument(
+    "--precision", type=str, default="fp16", help="Precision of Stable Diffusion"
+)
+parser.add_argument("--compile_to", type=str, help="torch, linalg, vmfb")
+parser.add_argument("--external_weight_path", type=str, default="")
+parser.add_argument(
+    "--external_weights",
+    type=str,
+    default=None,
+    help="saves ir/vmfb without global weights for size and readability, options [safetensors]",
+)
+parser.add_argument("--device", type=str, default="cpu", help="cpu, cuda, vulkan, rocm")
+# TODO: Bring in detection for target triple
+parser.add_argument(
+    "--iree_target_triple",
+    type=str,
+    default="",
+    help="Specify vulkan target triple or rocm/cuda target device.",
+)
+parser.add_argument("--vulkan_max_allocation", type=str, default="4294967296")
+parser.add_argument("--variant", type=str, default="decode")
+
+
+class VaeModel(torch.nn.Module):
+    def __init__(
+        self,
+        hf_model_name,
+        custom_vae="",
+    ):
+        super().__init__()
+        self.vae = None
+        if custom_vae in ["", None]:
+            self.vae = AutoencoderKL.from_pretrained(
+                hf_model_name,
+                subfolder="vae",
+            )
+        elif not isinstance(custom_vae, dict):
+            try:
+                # custom HF repo with no vae subfolder
+                self.vae = AutoencoderKL.from_pretrained(
+                    custom_vae,
+                )
+            except:
+                # some larger repo with vae subfolder
+                self.vae = AutoencoderKL.from_pretrained(
+                    custom_vae,
+                    subfolder="vae",
+                )
+        else:
+            # custom vae as a HF state dict
+            self.vae = AutoencoderKL.from_pretrained(
+                hf_model_name,
+                subfolder="vae",
+            )
+            self.vae.load_state_dict(custom_vae)
+
+    def decode_inp(self, inp):
+        inp = inp / 0.13025
+        x = self.vae.decode(inp, return_dict=False)[0]
+        x = (x / 2 + 0.5).clamp(0, 1)
+        return x
+
+    def encode_inp(self, inp):
+        latents = self.vae.encode(inp).latent_dist.sample()
+        return 0.13025 * latents
+
+
+def export_vae_model(
+    vae_model,
+    hf_model_name,
+    batch_size,
+    height,
+    width,
+    precision,
+    compile_to="torch",
+    external_weights=None,
+    external_weight_path=None,
+    device=None,
+    target_triple=None,
+    max_alloc=None,
+    variant="decode",
+):
+    mapper = {}
+    dtype = torch.float16 if precision == "f16" else torch.float32
+    if precision == "f16":
+        vae_model=vae_model.half()
+    utils.save_external_weights(
+        mapper, vae_model, external_weights, external_weight_path
+    )
+
+    sample = (batch_size, 4, height // 8, width // 8)
+    if variant == "encode":
+        sample = (batch_size, 3, height, width)
+
+    class CompiledVae(CompiledModule):
+        params = export_parameters(vae_model)
+
+        def main(self, inp=AbstractTensor(*sample, dtype=dtype)):
+            if variant == "decode":
+                return jittable(vae_model.decode_inp)(inp)
+            elif variant == "encode":
+                return jittable(vae_model.encode_inp)(inp)
+
+    import_to = "INPUT" if compile_to == "linalg" else "IMPORT"
+    inst = CompiledVae(context=Context(), import_to=import_to)
+
+    module_str = str(CompiledModule.get_mlir_module(inst))
+    safe_name = utils.create_safe_name(hf_model_name, f"-vae-{variant}")
+    if compile_to != "vmfb":
+        return module_str
+    else:
+        return utils.compile_to_vmfb(module_str, device, target_triple, max_alloc, safe_name)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    vae_model = VaeModel(
+        args.hf_model_name,
+    )
+    mod_str = export_vae_model(
+        vae_model,
+        args.hf_model_name,
+        args.batch_size,
+        args.height,
+        args.width,
+        args.precision,
+        args.compile_to,
+        args.external_weights,
+        args.external_weight_path,
+        args.device,
+        args.iree_target_triple,
+        args.vulkan_max_allocation,
+        args.variant,
+    )
+    safe_name = utils.create_safe_name(args.hf_model_name, "-vae")
+    with open(f"{safe_name}.mlir", "w+") as f:
+        f.write(mod_str)
+    print("Saved to", safe_name + ".mlir")
