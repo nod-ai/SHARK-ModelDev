009f18777b0cd0a325c66231916a3b7242554719
Quinn Dawkins <quinn@nod-labs.com>
2024-02-07 12:20:13 -0500

    Bump IREE to 20240207.794 and re-enable constexpr hoisting

--- models/turbine_models/custom_models/llama-benchmark/benchmark_module.py
+++ models/turbine_models/custom_models/llama-benchmark/benchmark_module.py
@@ -35,7 +35,6 @@ def create_benchmark_vmfb(args):
         "--iree-llvmcpu-target-triple=x86_64-linux-gnu",
         "--iree-stream-resource-index-bits=64",
         "--iree-vm-target-index-bits=64",
-        "--iree-opt-const-expr-hoisting=False",
     ]
     device = args.device
     if device == "cpu" or device == "llvm-cpu":
