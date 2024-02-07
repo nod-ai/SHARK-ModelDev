8fdc639ac9f64b4a9671235187544d4fb046f447
Ean Garvey <garveyej@gmail.com>
2024-02-07 10:14:47 -0600

    Move SDXL scripts.

---
+++ models/turbine_models/custom_models/sdxl_inference/clip_runner.py
@@ -0,0 +1,205 @@
+import argparse
+from turbine_models.model_runner import vmfbRunner
+from transformers import CLIPTokenizer
+from iree import runtime as ireert
+import torch
+
+parser = argparse.ArgumentParser()
+
+# TODO move common runner flags to generic flag file
+parser.add_argument(
+    "--vmfb_path_1", type=str, default="", help="path to vmfb containing compiled module"
+)
+parser.add_argument(
+    "--external_weight_path_1",
+    type=str,
+    default="",
+    help="path to external weight parameters if model compiled without them",
+)
+parser.add_argument(
+    "--vmfb_path_2", type=str, default="", help="path to vmfb containing compiled module"
+)
+parser.add_argument(
+    "--external_weight_path_2",
+    type=str,
+    default="",
+    help="path to external weight parameters if model compiled without them",
+)
+parser.add_argument(
+    "--compare_vs_torch",
+    action="store_true",
+    help="Runs both turbine vmfb and a torch model to compare results",
+)
+parser.add_argument(
+    "--hf_model_name",
+    type=str,
+    help="HF model name",
+    default="stabilityai/sdxl-turbo",
+)
+parser.add_argument(
+    "--hf_auth_token",
+    type=str,
+    help="The Hugging face auth token, required for some models",
+)
+parser.add_argument(
+    "--device",
+    type=str,
+    default="local-task",
+    help="local-sync, local-task, cuda, vulkan, rocm",
+)
+
+parser.add_argument(
+    "--prompt",
+    type=str,
+    default="a photograph of an astronaut riding a horse",
+    help="prompt for clip model",
+)
+parser.add_argument(
+    "--precision",
+    type=str,
+    default="f32",
+    help="f16, f32",
+)
+
+def run_clip(
+    device, prompt, vmfb_path, hf_model_name, hf_auth_token, external_weight_path, index
+):
+    runner = vmfbRunner(device, vmfb_path, external_weight_path)
+
+    tokenizer_1 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer",
+        token=hf_auth_token,
+    )
+    tokenizer_2 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer_2",
+        token=hf_auth_token,
+    )
+    if index==1:
+        text_input = tokenizer_1(
+            prompt,
+            padding="max_length",
+            max_length=tokenizer_1.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        example_input = text_input.input_ids
+        inp = [ireert.asdevicearray(runner.config.device, example_input)]
+        results = runner.ctx.modules.compiled_clip1["main"](*inp)
+    elif index==2:
+        text_input = tokenizer_2(
+            prompt,
+            padding="max_length",
+            max_length=tokenizer_2.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        example_input = text_input.input_ids
+        inp = [ireert.asdevicearray(runner.config.device, example_input)]
+        results = runner.ctx.modules.compiled_clip2["main"](*inp)
+    else: 
+        print("Incorrect CLIP model index, please use 1 or 2")
+        exit(1)
+
+    return results
+
+
+def run_torch_clip(hf_model_name, hf_auth_token, prompt, precision="f16"):
+    # TODO: Integrate with HFTransformerBuilder
+    from transformers import CLIPTextModel, CLIPTextModelWithProjection
+
+    model_1 = CLIPTextModel.from_pretrained(
+        hf_model_name,
+        subfolder="text_encoder",
+        token=hf_auth_token,
+    )
+    model_2 = CLIPTextModelWithProjection.from_pretrained(
+        hf_model_name,
+        subfolder="text_encoder_2",
+        token=hf_auth_token,
+    )
+    tokenizer_1 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer",
+        token=hf_auth_token,
+    )
+    tokenizer_2 = CLIPTokenizer.from_pretrained(
+        hf_model_name,
+        subfolder="tokenizer_2",
+        token=hf_auth_token,
+    )
+    text_input_1 = tokenizer_1(
+        prompt,
+        padding="max_length",
+        max_length=tokenizer_1.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    text_input_2 = tokenizer_2(
+        prompt,
+        padding="max_length",
+        max_length=tokenizer_2.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    example_input_1 = text_input_1.input_ids
+    example_input_2 = text_input_2.input_ids
+
+    results_1 = model_1.forward(example_input_1)[0]
+    results_2 = model_2.forward(example_input_2)[0]
+    np_torch_output_1 = results_1.detach().cpu().numpy()
+    np_torch_output_2 = results_2.detach().cpu().numpy()
+    return np_torch_output_1, np_torch_output_2
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    turbine_output1 = run_clip(
+        args.device,
+        args.prompt,
+        args.vmfb_path_1,
+        args.hf_model_name,
+        args.hf_auth_token,
+        args.external_weight_path_1,
+        index=1,
+    )
+    print(
+        "TURBINE OUTPUT 1:",
+        turbine_output1[0].to_host(),
+        turbine_output1[0].to_host().shape,
+        turbine_output1[0].to_host().dtype,
+    )
+
+    turbine_output2 = run_clip(
+        args.device,
+        args.prompt,
+        args.vmfb_path_2,
+        args.hf_model_name,
+        args.hf_auth_token,
+        args.external_weight_path_2,
+        index=2,
+    )
+    print(
+        "TURBINE OUTPUT 2:",
+        turbine_output2[0].to_host(),
+        turbine_output2[0].to_host().shape,
+        turbine_output2[0].to_host().dtype,
+    )
+    if args.compare_vs_torch:
+        print("generating torch output: ")
+        from turbine_models.custom_models.sdxl_inference import utils
+
+        torch_output1, torch_output2 = run_torch_clip(
+            args.hf_model_name, args.hf_auth_token, args.prompt, args.precision
+        )
+        print("TORCH OUTPUT 1:", torch_output1, torch_output1.shape, torch_output1.dtype)
+        err1 = utils.largest_error(torch_output1, turbine_output1[0])
+
+        print("TORCH OUTPUT 2:", torch_output2, torch_output2.shape, torch_output2.dtype)
+        err2 = utils.largest_error(torch_output2, turbine_output2[0])
+        print("Largest Error for CLIP 1: ", err1)
+        print("Largest Error for CLIP 2: ", err2)
+        assert err1 < 9e-5 and err2 < 9e-5
+    # TODO: Figure out why we occasionally segfault without unlinking output variables
+    turbine_output1, turbine_output2 = (None, None)
---
+++ models/turbine_models/custom_models/sdxl_inference/unet_runner.py
@@ -0,0 +1,200 @@
+import argparse
+from turbine_models.model_runner import vmfbRunner
+from transformers import CLIPTokenizer
+from iree import runtime as ireert
+import torch
+
+parser = argparse.ArgumentParser()
+
+# TODO move common runner flags to generic flag file
+parser.add_argument(
+    "--vmfb_path", type=str, default="", help="path to vmfb containing compiled module"
+)
+parser.add_argument(
+    "--external_weight_path",
+    type=str,
+    default="",
+    help="path to external weight parameters if model compiled without them",
+)
+parser.add_argument(
+    "--compare_vs_torch",
+    action="store_true",
+    help="Runs both turbine vmfb and a torch model to compare results",
+)
+parser.add_argument(
+    "--hf_model_name",
+    type=str,
+    help="HF model name",
+    default="stabilityai/sdxl-turbo",
+)
+parser.add_argument(
+    "--hf_auth_token",
+    type=str,
+    help="The Hugging face auth token, required for some models",
+)
+parser.add_argument(
+    "--device",
+    type=str,
+    default="local-task",
+    help="local-sync, local-task, cuda, vulkan, rocm",
+)
+parser.add_argument(
+    "--batch_size", type=int, default=1, help="Batch size for inference"
+)
+parser.add_argument(
+    "--height", type=int, default=512, help="Height of Stable Diffusion"
+)
+parser.add_argument("--width", type=int, default=512, help="Width of Stable Diffusion")
+parser.add_argument("--precision", type=str, default="f32", help="Precision of Stable Diffusion")
+
+
+def run_unet(
+    device,
+    sample,
+    timestep,
+    prompt_embeds,
+    text_embeds,
+    time_ids,
+    guidance_scale,
+    vmfb_path,
+    hf_model_name,
+    hf_auth_token,
+    external_weight_path,
+):
+    runner = vmfbRunner(device, vmfb_path, external_weight_path)
+
+    inputs = [
+        ireert.asdevicearray(runner.config.device, sample),
+        ireert.asdevicearray(runner.config.device, timestep),
+        ireert.asdevicearray(runner.config.device, prompt_embeds),
+        ireert.asdevicearray(runner.config.device, text_embeds),
+        ireert.asdevicearray(runner.config.device, time_ids),
+        ireert.asdevicearray(runner.config.device, guidance_scale),
+    ]
+    results = runner.ctx.modules.compiled_unet["main"](*inputs)
+    return results
+
+
+def run_torch_unet(
+    hf_model_name,
+    hf_auth_token,
+    sample,
+    timestep,
+    prompt_embeds,
+    text_embeds,
+    time_ids,
+    guidance_scale,
+):
+    from diffusers import UNet2DConditionModel
+
+    class UnetModel(torch.nn.Module):
+        def __init__(self, hf_model_name, hf_auth_token):
+            super().__init__()
+            self.unet = UNet2DConditionModel.from_pretrained(
+                hf_model_name,
+                subfolder="unet",
+                token=hf_auth_token,
+            )
+
+        def forward(
+                self,
+                sample,
+                timestep,
+                prompt_embeds,
+                text_embeds,
+                time_ids,
+                guidance_scale,
+        ):
+            with torch.no_grad():
+                added_cond_kwargs = {
+                    "text_embeds": text_embeds,
+                    "time_ids": time_ids,
+                }
+                samples = torch.cat([sample] * 2)
+                noise_pred = self.unet.forward(
+                    samples,
+                    timestep,
+                    encoder_hidden_states=prompt_embeds,
+                    cross_attention_kwargs=None,
+                    added_cond_kwargs=added_cond_kwargs,
+                    return_dict=False,
+                )[0]
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (
+                    noise_pred_text - noise_pred_uncond
+                )
+            return noise_pred
+
+    unet_model = UnetModel(
+        hf_model_name,
+        hf_auth_token,
+    )
+    results = unet_model.forward(
+        sample, timestep, prompt_embeds, text_embeds, time_ids, guidance_scale
+    )
+    np_torch_output = results.detach().cpu().numpy()
+    return np_torch_output
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.precision == "f16":
+        dtype = torch.float16
+    else:
+        dtype = torch.float32
+    sample = torch.rand(
+        args.batch_size, 4, args.height // 8, args.width // 8, dtype=dtype
+    )
+    timestep = torch.zeros(1, dtype=torch.int64)
+    prompt_embeds = torch.rand(
+        2 * args.batch_size, args.max_length, 2048, dtype=dtype
+    )
+    text_embeds = torch.rand(2 * args.batch_size, 1280, dtype=dtype)
+    time_ids = torch.zeros(2 * args.batch_size, 6, dtype=dtype)
+    guidance_scale = torch.Tensor([7.5], dtype=dtype)
+    if args.hf_model_name == "CompVis/stable-diffusion-v1-4":
+        encoder_hidden_states = torch.rand(2, 77, 768, dtype=dtype)
+    elif args.hf_model_name == "stabilityai/stable-diffusion-2-1-base":
+        encoder_hidden_states = torch.rand(2, 77, 1024, dtype=dtype)
+
+    turbine_output = run_unet(
+        args.device,
+        sample,
+        timestep,
+        prompt_embeds,
+        text_embeds,
+        time_ids,
+        guidance_scale,
+        args.vmfb_path,
+        args.hf_model_name,
+        args.hf_auth_token,
+        args.external_weight_path,
+    )
+    print(
+        "TURBINE OUTPUT:",
+        turbine_output.to_host(),
+        turbine_output.to_host().shape,
+        turbine_output.to_host().dtype,
+    )
+
+    if args.compare_vs_torch:
+        print("generating torch output: ")
+        from turbine_models.custom_models.sd_inference import utils
+
+        torch_output = run_torch_unet(
+            args.hf_model_name,
+            args.hf_auth_token,
+            sample,
+            timestep,
+            prompt_embeds,
+            text_embeds,
+            time_ids,
+            guidance_scale,
+        )
+        print("TORCH OUTPUT:", torch_output, torch_output.shape, torch_output.dtype)
+        err = utils.largest_error(torch_output, turbine_output)
+        print("Largest Error: ", err)
+        assert err < 9e-5
+
+    # TODO: Figure out why we occasionally segfault without unlinking output variables
+    turbine_output = None
