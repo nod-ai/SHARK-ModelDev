c23f67eeefd5b5c4c67cd4f09cf641abeb57be0b
Stella Laurenzo <stellaraccident@gmail.com>
2024-02-29 14:52:43 -0800

    Save prefill mlir.

--- llm/scripts/validate_paged_llama_model.py
+++ llm/scripts/validate_paged_llama_model.py
@@ -92,6 +92,41 @@ def main(args: list[str]):
     print(f"  : cache[0] = {cache_state[0][0]}")
     print(f"  : cache[1] = {cache_state[0][1]}")
 
+    def save_prefill_module(model):
+        from shark_turbine.importers.fx_importer import FxImporter
+        from iree.compiler.ir import AsmState
+
+        importer = FxImporter()
+        # asm_state = AsmState(importer.module_op)
+
+        print("Generating FX graph")
+
+        class InferenceModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.add_module("prefill", model)
+
+            def forward(self, next_batch, attention_mask, seq_block_ids, *cache_state):
+                return self.prefill.prefill(
+                    next_batch,
+                    attention_mask=attention_mask,
+                    seq_block_ids=seq_block_ids,
+                    cache_state=list(cache_state),
+                )
+
+        infmod = InferenceModule()
+        prog = torch.export.export(
+            infmod, (next_batch, attention_mask, seq_block_ids) + tuple(cache_state)
+        )
+
+        print(f"FX prog:", prog)
+        importer.import_program(prog, func_name="prefill")
+        output_file = "/tmp/prefill.mlirbc"
+        print("Saving to:", output_file)
+        with open(output_file, "wb") as f:
+            importer.module_op.write_bytecode(f)
+
+    save_prefill_module()
 
 
 if __name__ == "__main__":
