eecab906540b5fe2abb13636bf77e642868b2c69
Ean Garvey <garveyej@gmail.com>
2024-03-01 11:25:17 -0600

    Explicitly set some types in pytest args.

--- models/turbine_models/tests/conftest.py
+++ models/turbine_models/tests/conftest.py
@@ -10,12 +10,12 @@ def pytest_addoption(parser):
         action="store",
         default="stable_diffusion_xl_base_1_0",
     )
-    parser.addoption("--batch_size", action="store", default=1)
-    parser.addoption("--height", action="store", default=1024)
-    parser.addoption("--width", action="store", default=1024)
+    parser.addoption("--batch_size", type=int, action="store", default=1)
+    parser.addoption("--height", type=int, action="store", default=1024)
+    parser.addoption("--width", type=int, action="store", default=1024)
     parser.addoption("--precision", action="store", default="fp16")
-    parser.addoption("--max_length", action="store", default=64)
-    parser.addoption("--guidance_scale", action="store", default=7.5)
+    parser.addoption("--max_length", type=int, action="store", default=64)
+    parser.addoption("--guidance_scale", type=float, action="store", default=7.5)
     parser.addoption("--run_vmfb", action="store", default=True)
     parser.addoption("--compile_to", action="store", default=None)
     parser.addoption("--vmfb_path", action="store", default="")
@@ -24,7 +24,9 @@ def pytest_addoption(parser):
     parser.addoption("--device", action="store", default="cpu")
     parser.addoption("--rt_device", action="store", default="local-task")
     parser.addoption("--iree_target_triple", action="store", default="x86_64-linux-gnu")
-    parser.addoption("--vulkan_max_allocation", action="store", default="4294967296")
+    parser.addoption(
+        "--vulkan_max_allocation", type=int, action="store", default="4294967296"
+    )
     parser.addoption(
         "--prompt",
         action="store",
@@ -35,8 +37,8 @@ def pytest_addoption(parser):
         action="store",
         default="blurry, unsaturated, watermark, noisy, grainy, out of focus",
     )
-    parser.addoption("--in_channels", action="store", default=4)
-    parser.addoption("--num_inference_steps", action="store", default=35)
-    parser.addoption("--benchmark", action="store", default=False)
+    parser.addoption("--in_channels", type=int, action="store", default=4)
+    parser.addoption("--num_inference_steps", type=int, action="store", default=35)
+    parser.addoption("--benchmark", action="store_true", default=False)
     parser.addoption("--decomp_attn", action="store_true", default=False)
-    parser.addoption("--tracy_profile", action="store", default=False)
+    parser.addoption("--tracy_profile", action="store_true", default=False)
