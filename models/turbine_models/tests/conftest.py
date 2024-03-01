f2cc90b25688fe6d017de87b5b9c50ab2b8efae0
Ean Garvey <garveyej@gmail.com>
2024-03-01 12:23:20 -0600

    Explicitly set target triple flag to string type.

--- models/turbine_models/tests/conftest.py
+++ models/turbine_models/tests/conftest.py
@@ -23,7 +23,7 @@ def pytest_addoption(parser):
     parser.addoption("--external_weight_path", action="store", default="")
     parser.addoption("--device", action="store", default="cpu")
     parser.addoption("--rt_device", action="store", default="local-task")
-    parser.addoption("--iree_target_triple", action="store", default="x86_64-linux-gnu")
+    parser.addoption("--iree_target_triple", type="str", action="store", default="x86_64-linux-gnu")
     parser.addoption(
         "--vulkan_max_allocation", type=int, action="store", default="4294967296"
     )
