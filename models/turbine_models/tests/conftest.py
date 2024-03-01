92617d028f0e5c8518cda779d99c964cebaf85e5
jinchen62 <jinchenye62@gmail.com>
2024-03-01 09:48:50 -0800

    fix sdxl CI

--- models/turbine_models/tests/conftest.py
+++ models/turbine_models/tests/conftest.py
@@ -24,10 +24,10 @@ def pytest_addoption(parser):
     parser.addoption("--device", action="store", default="cpu")
     parser.addoption("--rt_device", action="store", default="local-task")
     parser.addoption(
-        "--iree_target_triple", type="str", action="store", default="x86_64-linux-gnu"
+        "--iree_target_triple", type=str, action="store", default="x86_64-linux-gnu"
     )
     parser.addoption(
-        "--vulkan_max_allocation", type=int, action="store", default="4294967296"
+        "--vulkan_max_allocation", type=str, action="store", default="4294967296"
     )
     parser.addoption(
         "--prompt",
