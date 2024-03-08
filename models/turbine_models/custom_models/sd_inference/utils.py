d71ceb165aca4d9cdf54dfdc165efe1ead79fe18
Ean Garvey <garveyej@gmail.com>
2024-03-07 22:28:01 -0600

    Remove source map stripping flag from rocm compile args

--- models/turbine_models/custom_models/sd_inference/utils.py
+++ models/turbine_models/custom_models/sd_inference/utils.py
@@ -78,7 +78,6 @@ def compile_to_vmfb(
                 "--iree-hal-target-backends=rocm",
                 "--iree-rocm-target-chip=" + target_triple,
                 "--iree-rocm-link-bc=true",
-                "--iree-vm-bytecode-module-strip-source-map=true",
                 "--iree-vm-target-truncate-unsupported-floats",
                 "--iree-flow-inline-constants-max-byte-length=1",
             ]
