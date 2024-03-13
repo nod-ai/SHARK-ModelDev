b2d3398a691330037e62b9ee5472dfa46d77d122
Ean Garvey <garveyej@gmail.com>
2024-03-13 12:15:14 -0500

    Fixes to pipeline, cooler prompt, fix scheduled unet comparisons

--- models/turbine_models/custom_models/sdxl_inference/sdxl_cmd_opts.py
+++ models/turbine_models/custom_models/sdxl_inference/sdxl_cmd_opts.py
@@ -58,7 +58,7 @@ p.add_argument(
 p.add_argument(
     "--prompt",
     type=str,
-    default="A very fast car leaving a trail of fire as it screams along a mountain road, old school racing animation, retro 1980s anime style, 4k",
+    default="A very fast car leaving a trail of fire as it screams along a mountain road, old school racing animation, retro 1980s anime style, 4k, motion blur, action shot, semi-realistic, nightwave, neon, tokyo",
     help="Prompt input to stable diffusion.",
 )
 
