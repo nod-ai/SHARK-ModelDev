bfbebefee2393f7c5987d1f600b34626e3a0af31
Ean Garvey <garveyej@gmail.com>
2024-04-09 23:55:24 -0500

    xfail e2e on rocm shortly, pending move to nightly test

--- models/turbine_models/tests/sdxl_test.py
+++ models/turbine_models/tests/sdxl_test.py
@@ -502,7 +502,7 @@ class StableDiffusionXLTest(unittest.TestCase):
         np.testing.assert_allclose(torch_output, turbine, rtol, atol)
 
     def test05_t2i_generate_images(self):
-        if arguments["device"] in ["vulkan", "cuda"]:
+        if arguments["device"] in ["vulkan", "cuda", "rocm"]:
             self.skipTest("Have issues with submodels on these backends")
         mlirs = {
             "vae_decode": None,
