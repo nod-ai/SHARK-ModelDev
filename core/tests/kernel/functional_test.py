31a1d6ef03eafdbcc53f415ecfc15b7b9f0e1334
Martin Lücke <martin.luecke@ed.ac.uk>
2024-04-10 19:10:19 +0200

    Adjust functional_test to latest discussions

--- core/tests/kernel/functional_test.py
+++ core/tests/kernel/functional_test.py
@@ -5,6 +5,7 @@ import shark_turbine.kernel as tk
 import shark_turbine.kernel.lang as tkl
 import shark_turbine.kernel.functional as tkf
 
+
 class Test(unittest.TestCase):
     def testGemm(self):
         # Tensor dimensions
@@ -37,28 +38,51 @@ class Test(unittest.TestCase):
         # explicit notion of a warp id (like a workgroup or thread id).
         # Here we use a functional style of expressing the loop since
         # we do not know the loop bounds.
-        #@tkf.wave(constraints)
-        @tkf.wave()
-        def gemm(a: tkf.Memory[M, K, ADDRESS_SPACE, tkl.f16],
-                 b: tkf.Memory[N, K, ADDRESS_SPACE, tkl.f16],
-                 c: tkf.Memory[M, N, ADDRESS_SPACE, tkl.f32]):
+        # @tkf.wave(constraints)
+        @tkf.wave(N // BLOCK_N, M // BLOCK_M)
+        def gemm(
+            a: tkf.Memory[M, K, ADDRESS_SPACE, tkl.f16],
+            b: tkf.Memory[N, K, ADDRESS_SPACE, tkl.f16],
+            c: tkf.Memory[M, N, ADDRESS_SPACE, tkl.f32],
+        ):
             # This microkernel encodes the fact that if the reduction
             # dimension were tiled, then we would need to materialize a loop.
             @tkf.tiledLoop(K)
-            def repeat(c_reg : tkf.Register[WAVE_M, WAVE_N, tkl.f32] = 0) -> tkf.Register[WAVE_M, WAVE_N, tkl.f32]:
-                a_reg: tkf.Register[WAVE_M, WAVE_K, tkl.f16] = tkf.read(a, elements_per_thread=LOAD_ELEMS_PER_THREAD)
-                b_reg: tkf.Register[WAVE_N, WAVE_K, tkl.f16] = tkf.read(b, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+            def repeat(
+                c_reg: tkf.Register[WAVE_M, WAVE_N, tkl.f32] = 0
+            ) -> tkf.Register[WAVE_M, WAVE_N, tkl.f32]:
+                # a_reg: tkf.Register[WAVE_M, WAVE_K, tkl.f16]
+                # b_reg: tkf.Register[WAVE_N, WAVE_K, tkl.f16]
+                a_reg = tkf.read(a, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+                b_reg = tkf.read(b, elements_per_thread=LOAD_ELEMS_PER_THREAD)
                 c_reg = tkf.mma(a_reg, b_reg, c_reg)
                 return c_reg
-            tkf.write(repeat, c, elements_per_thread=STORE_ELEMS_PER_THREAD)
 
-        hyperparams = {ADDRESS_SPACE:1, LOAD_ELEMS_PER_THREAD:4, STORE_ELEMS_PER_THREAD:1, BLOCK_M: 32, BLOCK_N: 128, BLOCK_K: 64,
-                       WAVE_M:16, WAVE_N:16, WAVE_K:16}
+            result = repeat()
+            tkf.write(result, c, elements_per_thread=STORE_ELEMS_PER_THREAD)
+            # We also discussed using `repeat` directly in tkf.write:
+            # tkf.write(repeat, c, elements_per_thread=STORE_ELEMS_PER_THREAD)
+
+        hyperparams = {
+            ADDRESS_SPACE: 1,
+            LOAD_ELEMS_PER_THREAD: 4,
+            STORE_ELEMS_PER_THREAD: 1,
+            BLOCK_M: 32,
+            BLOCK_N: 128,
+            BLOCK_K: 64,
+            WAVE_M: 16,
+            WAVE_N: 16,
+            WAVE_K: 16,
+            M: 64,
+            N: 256,
+            K: 128,
+        }
         with tk.gen.TestLaunchContext(hyperparams):
             a = torch.randn(64, 128, dtype=torch.float16)
             b = torch.randn(256, 128, dtype=torch.float16)
             c = torch.zeros(64, 256, dtype=torch.float32)
             gemm(a, b, c)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
