1bb4c83de22b0d8b6b40eb5eec2629a0e27712f9
Scott Todd <scott.todd0@gmail.com>
2024-04-16 10:20:59 -0700

    Documentation touchups to README and package files.

--- core/iree/turbine/__init__.py
+++ core/iree/turbine/__init__.py
@@ -1,3 +1,8 @@
+"""
+The turbine package provides development tools for deploying PyTorch 2 machine
+learning models to cloud and edge devices.
+"""
+
 # Copyright 2024 Advanced Micro Devices, Inc.
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
