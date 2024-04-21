1ddecbbe16abea93429731747e1408e0f96e4d6c
Stella Laurenzo <stellaraccident@gmail.com>
2024-04-20 20:42:59 -0700

    Delete llm and serving directories. (#644)
    They have been promoted to a dedicated project:
    https://github.com/nod-ai/sharktank

--- llm/turbine_llm/layers/configs/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2024 Advanced Micro Devices, Inc
-#
-# Licensed under the Apache License v2.0 with LLVM Exceptions.
-# See https://llvm.org/LICENSE.txt for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-from .llm_configs import LlamaHParams
