ef6f3d087b6fbd4efa4dd2e46c1d95dd58d36498
Ean Garvey <garveyej@gmail.com>
2024-05-16 19:51:10 -0500

    Revert change to clip export function signature

--- models/turbine_models/custom_models/sdxl_inference/sdxl_prompt_encoder.py
+++ models/turbine_models/custom_models/sdxl_inference/sdxl_prompt_encoder.py
@@ -102,7 +102,6 @@ class PromptEncoderModule(torch.nn.Module):
 def export_prompt_encoder(
     hf_model_name,
     hf_auth_token=None,
-    batch_size=1,
     max_length=64,
     precision="fp16",
     compile_to="torch",
@@ -125,7 +124,7 @@ def export_prompt_encoder(
         safe_name = os.path.join(pipeline_dir, "prompt_encoder")
     else:
         safe_name = utils.create_safe_name(
-            hf_model_name, f"-bs{batch_size}-{str(max_length)}-{precision}-prompt-encoder-{device}"
+            hf_model_name, f"{str(max_length)}-{precision}-prompt-encoder-{device}"
         )
     if input_mlir:
         vmfb_path = utils.compile_to_vmfb(
@@ -234,7 +233,7 @@ if __name__ == "__main__":
     if args.input_mlir:
         exit()
     safe_name_1 = safe_name = utils.create_safe_name(
-        args.hf_model_name, f"_bs{str(args.batch_size)}_{str(args.max_length)}_{args.precision}_prompt_encoder"
+        args.hf_model_name, f"{str(args.max_length)}_{args.precision}_prompt_encoder"
     )
     with open(f"{safe_name}.mlir", "w+") as f:
         f.write(mod_str)
