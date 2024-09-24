0291d43ef515e40240734ac58edef73eaad2583e
Ean Garvey <ean.garvey@amd.com>
2024-09-24 11:30:59 -0500

    Small fixes to SDXL inference pipeline/exports/compile

--- models/turbine_models/custom_models/sd_inference/vae.py
+++ models/turbine_models/custom_models/sd_inference/vae.py
@@ -171,7 +171,6 @@ def export_vae_model(
             vae_model,
             external_weights,
             external_weight_path,
-            vae_harness=vae_harness,
         )
     if weights_only:
         return external_weight_path
