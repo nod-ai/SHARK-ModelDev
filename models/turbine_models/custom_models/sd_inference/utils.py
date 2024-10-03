dfb9474e0f0b5955b338e22dee7d2c6da1ad7a7f
Ean Garvey <ean.garvey@amd.com>
2024-10-03 13:57:48 -0500

    Remove default/mfma/wmma specs from sd compile utils.

--- models/turbine_models/custom_models/sd_inference/utils.py
+++ models/turbine_models/custom_models/sd_inference/utils.py
@@ -347,27 +347,8 @@ def compile_to_vmfb(
     # This is a temporary solution, and should be removed or largely disabled once the functionality of
     # the TD spec is implemented in C++.
 
-    if attn_spec in ["default", "mfma", "punet"]:
-        #        if any(x in safe_name for x in ["clip", "prompt_encoder"]) == False:
-        use_punet = True if attn_spec in ["punet", "i8"] else False
-        attn_spec = get_mfma_spec_path(
-            target_triple,
-            os.path.dirname(safe_name),
-            use_punet=use_punet,
-            masked_attention=masked_attention,
-        )
-        if attn_spec:
-            flags.extend(["--iree-codegen-transform-dialect-library=" + attn_spec])
-
-    elif attn_spec in ["wmma"] or ("gfx11" in target_triple and not attn_spec):
-        attn_spec = get_wmma_spec_path(
-            target_triple, os.path.dirname(safe_name), masked_attention=masked_attention
-        )
-        if attn_spec:
-            flags.extend(["--iree-codegen-transform-dialect-library=" + attn_spec])
-    elif attn_spec and attn_spec != "None":
-        if any(x in safe_name for x in ["clip", "prompt_encoder"]) == False:
-            flags.extend(["--iree-codegen-transform-dialect-library=" + attn_spec])
+    if os.path.exists(attn_spec):
+        flags.extend(["--iree-codegen-transform-dialect-library=" + attn_spec])
 
     for i, flag in enumerate(ireec_flags):
         k = flag.strip().split("=")[0]
